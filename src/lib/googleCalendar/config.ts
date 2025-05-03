ff428cd6e1568f0d66eaa31c307e946154a9eeab
JoaoLops3 <joaogabriellops2353@gmail.com>
2025-05-03 10:48:46 -0300

    refactor: move Google Calendar credentials to environment variables

--- src/lib/googleCalendar/config.ts
+++ src/lib/googleCalendar/config.ts
@@ -1,15 +1,16 @@
 export const GOOGLE_CALENDAR_CONFIG = {
-  apiKey: 'AIzaSyBxQwXQwXQwXQwXQwXQwXQwXQwXQwXQwXQ',
-  clientId: '123456789012-abcdefghijklmnopqrstuvwxyz123456.apps.googleusercontent.com',
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY || '',
+  clientId: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_CLIENT_ID || '',
   scopes: [
     'https://www.googleapis.com/auth/calendar',
-    'https://www.googleapis.com/auth/calendar.readonly',
     'https://www.googleapis.com/auth/calendar.events',
-    'https://www.googleapis.com/auth/calendar.events.readonly',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email'
+    'https://www.googleapis.com/auth/tasks',
+    'https://www.googleapis.com/auth/tasks.readonly'
   ],
-  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  discoveryDocs: [
+    'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+    'https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'
+  ],
   redirectUri: 'http://localhost:5173/auth/callback',
   authorizedOrigins: ['http://localhost:5173'],
   authorizedRedirectUris: ['http://localhost:5173/auth/callback']
